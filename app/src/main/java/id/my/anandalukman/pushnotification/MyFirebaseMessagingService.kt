1cdbcb06dd863c51f9cee1cf4540997126b3f202
Ananda Lukman <ananda.muhamad.lukman@gmail.com>
2022-01-11 22:41:26 +0700

    Adding Firebase Configuration in AndroidManifest.xml , Gradle, and class, and logo

---
+++ app/src/main/java/id/my/anandalukman/pushnotification/MyFirebaseMessagingService.kt
@@ -0,0 +1,13 @@
+package id.my.anandalukman.pushnotification
+
+import com.google.firebase.messaging.FirebaseMessagingService
+
+class MyFirebaseMessagingService : FirebaseMessagingService() {
+
+    /** --- Step --- **\
+     * Generate the notification
+     * attach the notification created with the custom layout
+     * show notification
+     */
+
+}
