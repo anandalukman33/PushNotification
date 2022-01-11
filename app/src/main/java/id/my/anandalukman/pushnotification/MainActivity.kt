ad7afbecf8add601de61738604d2b802a4a5ef2d
Ananda Lukman <ananda.muhamad.lukman@gmail.com>
2022-01-11 21:47:07 +0700

    Initial Commit and Penambahan Google Service Json di Folder App

---
+++ app/src/main/java/id/my/anandalukman/pushnotification/MainActivity.kt
@@ -0,0 +1,11 @@
+package id.my.anandalukman.pushnotification
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
